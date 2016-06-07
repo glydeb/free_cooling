bf6ad25aa2d2a3e2c53b0e7c8dcb07eb714bcf71
Glyde <glydeb@yahoo.com>
2016-06-07 16:29:34 -0500

    add start script, connection string, and start setup form layout

--- server/routes/setup.js
+++ server/routes/setup.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/free_cooling';
+
 /*
 router.get('/', function (req, res) {
   Pet.find({}, function (err, movies) {
