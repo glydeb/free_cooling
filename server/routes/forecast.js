df7e62d4d196f7683c5df191f96a8e916903ba66
Glyde <glydeb@yahoo.com>
2016-06-14 21:35:42 -0500

    set forecast api key properly

--- server/routes/forecast.js
+++ server/routes/forecast.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var request = require('request');
 var Forecast = require('forecast.io-bluebird');
 var forecast = new Forecast({
-  key: process.env.FORECAST_API_KEY,
+  key: process.env.FORECAST_KEY,
   timeout: 2500
 });
 
