29af6e3f236b263ba915634cd8f9535508ee07fa
Glyde <glydeb@yahoo.com>
2016-06-18 23:22:22 -0500

    require request

--- server/routes/alert.js
+++ server/routes/alert.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 var Promise = require('bluebird');
 Promise.promisifyAll(require('request'));
 var pg = require('pg');
